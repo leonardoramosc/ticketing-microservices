e6f1c79f96b1afcb9b68bbaec6268e54e2670c16
Leonardo Ramos Cabrera <leorrc96@gmail.com>
2024-04-22 22:00:19 -0300

    new startup messages (#3)
    new startup messages

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,22 +2,27 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log("starting...");
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error('NATS_CLIENT_ID must be defined');
+    throw new Error("NATS_CLIENT_ID must be defined");
   }
 
   if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined');
+    throw new Error("NATS_URL must be defined");
   }
 
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error('NATS_CLUSTER_ID must be defined');
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
   try {
-    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL)
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
 
-    new OrderCreatedListener(natsWrapper.client).listen()
+    new OrderCreatedListener(natsWrapper.client).listen();
 
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
